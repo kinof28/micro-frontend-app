5363274df0a8cb4180c0e4616e86d342de831a3b
Abdelwahab <ab28fb@gmail.com>
2022-04-29 01:54:35 +0100

    initial commit

---
+++ apps/shell/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: 'gallery',
+          loadChildren: () =>
+            import('gallery/Module').then((m) => m.RemoteEntryModule),
+        },
+      ],
+      { initialNavigation: 'enabledBlocking' }
+    ),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
